2f247325bbd661ccbf0fed44909acd3dd25ec45d
Vyacheslav Frolov <crtdlunacy@gmail.com>
2023-02-12 14:31:53 +0400

    add ErrBoundary component

---
+++ src/app/providers/ErrorBoudary/ui/ErrorBoudary.tsx
@@ -0,0 +1,45 @@
+// eslint-disable-next-line no-undef
+import React, { ErrorInfo, ReactNode, Suspense } from 'react';
+import { withTranslation } from 'react-i18next';
+import { PageError } from 'widgets/PageError';
+
+interface ErrorBoudaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary
+    extends React.Component<ErrorBoudaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoudaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // You can also log the error to an error reporting service
+        console.log(error, errorInfo);
+    }
+
+    render() {
+        const { hasError } = this.state;
+        const { children } = this.props;
+        if (hasError) {
+            // You can render any custom fallback UI
+            return (
+                <Suspense fallback="">
+                    <PageError />
+                </Suspense>
+            );
+        }
+
+        return children;
+    }
+}
---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,25 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import './Loader.scss';
+
+interface LoaderProps {
+  className?: string
+}
+
+const Loader = ({ className }: LoaderProps) => (
+    <div className={classNames(
+        'loadingio-spinner-ellipsis-uw59fjnr19',
+        {},
+        [className],
+    )}
+    >
+        <div className="ldio-9pnd59mcdtk">
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+        </div>
+    </div>
+);
+
+export default Loader;
