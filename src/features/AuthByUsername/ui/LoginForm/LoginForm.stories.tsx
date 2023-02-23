b2709cc746ad715fc170861d994fbaea1d311539
Vyacheslav Frolov <crtdlunacy@gmail.com>
2023-02-23 18:12:44 +0400

    Add stories

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import { Theme } from 'app/providers/ThemeProvider';
 import LoginForm from './LoginForm';
 
 export default {
@@ -17,8 +15,3 @@ const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args
 export const Light = Template.bind({});
 Light.args = {
 };
-
-export const Dark = Template.bind({});
-Dark.args = {
-};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
