1c26b240d5c95e46e46fb2c1a6369546da65f6d0
CrtdLunacy <crtdlunacy@gmail.com>
2023-07-04 10:47:36 +0300

    add Drawer for Notifications

--- src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
@@ -6,12 +6,15 @@ describe('SideBar Component Test', () => {
     test('Render button with text', () => {
         componentRender(<SideBar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
     });
 
     test('Toggle sidebar', () => {
         componentRender(<SideBar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
+        fireEvent.click(toggleBtn);
         fireEvent.click(toggleBtn);
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
     });
