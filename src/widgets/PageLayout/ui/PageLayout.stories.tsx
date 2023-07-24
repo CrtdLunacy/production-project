d0d4907974af2b14c6dbb054f1746cf9487f901a
CrtdLunacy <crtdlunacy@gmail.com>
2023-07-24 15:03:04 +0300

    update eslint, update lunacy package, fix errors

--- src/widgets/PageLayout/ui/PageLayout.stories.tsx
+++ src/widgets/PageLayout/ui/PageLayout.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from '@/app/providers/ThemeProvider';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
-import PageLayout from './PageLayout';
+import { PageLayout } from './PageLayout';
 
 export default {
     title: 'shared/PageLayout',
